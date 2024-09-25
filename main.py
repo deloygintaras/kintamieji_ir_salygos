687c01dcc6accebf7a60559563b9af0701475b15
deloygintaras <turcinasgintaras@gmail.com>
2024-09-25 21:26:51 +0300

    tesinys

--- main.py
+++ main.py
@@ -49,5 +49,70 @@ import random
 # middle_value = number[1]
 # print(f"Vidurine reiksme: {middle_value}")
 
-# Įvedami skaičiai - a, b, c –kraštinių ilgiai, trys kintamieji (naudokite random.randint(x,x) funkciją nuo 1 iki 10).
+
+# 4. Įvedami skaičiai - a, b, c –kraštinių ilgiai, trys kintamieji (naudokite random.randint(x,x) funkciją nuo 1 iki 10).
 # Parašykite Python programą, kuri nustatytų, ar galima sudaryti trikampį ir atsakymą atspausdintų.
+
+# a = random.randint(1, 10)
+# b = random.randint(1, 10)
+# c = random.randint(1, 10)
+# print(f"a = {a} \nb = {b} \nc = {c}")
+# if (a + b > c) and (a + c > b) and (b + c > a):
+#     print("trikampi sudaryti galima")
+# else:
+#     print("trikampi sudaryti neimanoma")
+
+# 5. Sukurkite keturis kintamuosius ir random.randint(x,x) funkcija sugeneruokite jiems reikšmes nuo 0 iki 2.
+# Suskaičiuokite kiek yra nulių, vienetų ir dvejetų. (sprendimui masyvo nenaudoti).
+
+# a = random.randint(0, 2)
+# b = random.randint(0, 2)
+# c = random.randint(0, 2)
+# d = random.randint(0, 2)
+# count0 = 0
+# count1 = 0
+# count2 = 0
+# print(f"a = {a} \nb = {b} \nc = {c} \nd = {d}")
+# if a == 0:
+#     count0 = count0 + 1
+# elif a == 1:
+#     count1 = count1 + 1
+# elif a == 2:
+#     count2 = count2 + 1
+# if b == 0:
+#     count0 = count0 + 1
+# elif b == 1:
+#     count1 = count1 + 1
+# elif b == 2:
+#     count2 = count2 + 1
+# if c == 0:
+#     count0 = count0 + 1
+# elif c == 1:
+#     count1 = count1 + 1
+# elif c == 2:
+#     count2 = count2 + 1
+# if d == 0:
+#     count0 = count0 + 1
+# elif d == 1:
+#     count1 = count1 + 1
+# elif d == 2:
+#     count2 = count2 + 1
+# print(f" 0 yra:{count0}")
+# print(f" 1 yra:{count1}")
+# print(f" 2 yra:{count2}")
+
+
+# antra variantas
+# a = random.randint(0, 2)
+# b = random.randint(0, 2)
+# c = random.randint(0, 2)
+# d = random.randint(0, 2)
+# print(f"a = {a} \nb = {b} \nc = {c} \nd = {d}")
+# skaiciai = [a, b, c, d]
+# nuliu = skaiciai.count(0)
+# vienetu = skaiciai.count(1)
+# dvejetu = skaiciai.count(2)
+# print(f" 0 yra:{nuliu}")
+# print(f" 1 yra:{vienetu}")
+# print(f" 2 yra:{dvejetu}")
+
