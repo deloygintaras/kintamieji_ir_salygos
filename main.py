3b9de29e97c01c623c2565e084ad835dfb421ba0
deloygintaras <turcinasgintaras@gmail.com>
2024-09-26 20:01:03 +0300

    belaikis

--- main.py
+++ main.py
@@ -143,9 +143,131 @@ import random
 # elif c > 0:
 #     print(f"c skaicius = {{{c}}}")
 
-# Įmonė parduoda žvakes po 1 EUR. Perkant daugiau kaip 1000 vienetų taikoma 3 % nuolaida, daugiau kaip 2000 vienetų- 4 % nuolaida.
+# 7. Įmonė parduoda žvakes po 1 EUR. Perkant daugiau kaip 1000 vienetų taikoma 3 % nuolaida, daugiau kaip 2000 vienetų- 4 % nuolaida.
 # Parašykite programą, kuri skaičiuos žvakių kainą ir atspausdintų atsakymą kiek žvakių ir kokia kaina perkama.
 # Žvakių kiekį generuokite random.randint(x,x) funkcija nuo 5 iki 3000.
 
-a = random.randint(5, 3000)
-print(f"zvakiu kiekis = {a}")
+# a = random.randint(5, 3000)
+# print(f"zvakiu kiekis = {a}")
+# kaina = a * 1
+# print(f"Bendra zvakiu kaina = {kaina}Eur")
+# if a > 2000:
+#     print(f"Kaina perkant daugiau kaip 2000 vienetu bus sumoketa (su 4 % nuolaida): {a}*0.96 = {round(kaina * 0.96, 2)}Eur.")
+# elif a > 1000:
+#     print(f"Kaina perkant daugiau kaip 1000 vienetu bus sumoketa (su 3 % nuolaida): {a}*0.97 = {round(kaina * 0.97, 2)}Eur.")
+# else:
+#     print(f"Nuolaida netaikoma, iprasta kaina = {kaina}Eur")
+
+# 8. Naudokite funkcija random.randint(x,x). Sukurkite tris kintamuosius su atsitiktinėm reikšmėm nuo 0 iki 100.
+# Paskaičiuokite jų aritmetinį vidurkį. Ir aritmetinį vidurkį atmetus tas reikšmes, kurios yra mažesnės nei 10 arba didesnės nei 90.
+# Abu vidurkius atspausdinkite. Rezultatus apvalinkite iki sveiko skaičiaus.
+
+# a = random.randint(0, 100)
+# b = random.randint(0, 100)
+# c = random.randint(0, 100)
+# print(f"a = {a} \nb = {b} \nc = {c}")
+# vidurkis = (a + b + c) / 3
+# print(f"aritmetinis vidurkis = {round(vidurkis)}")
+# fm = 0
+# fk = 0
+# if 10 <= a <= 90:
+#     fm += a
+#     fk += 1
+# if 10 <= b <= 90:
+#     fm += b
+#     fk += 1
+# if 10 <= c <= 90:
+#     fm += c
+#     fk += 1
+# if fk > 0:
+#     vidurkis = fm / fk
+#     print(f"{round(fm / fk, 0)}")
+
+# 9. Padarykite skaitmeninį laikrodį, rodantį valandas, minutes ir sekundes.
+# Valandom, minutėm ir sekundėm sugeneruoti panaudokite funkciją random.randint(x,x).
+# Sugeneruokite skaičių nuo 0 iki 300. Tai papildomos sekundės. Skaičių pridėkite prie jau sugeneruoto laiko.
+# Atspausdinkite laikrodį prieš ir po sekundžių pridėjimo ir pridedamų sekundžių skaičių.
+
+
+
+#  10. Naudokite funkcija random.randint(x,x). Sugeneruokite 6 kintamuosius su atsitiktinem reikšmėm nuo 1000 iki 9999.
+# Atspausdinkite reikšmes viename strige, išrūšiuotas nuo didžiausios iki mažiausios, atskirtas tarpais. Naudoti ciklų ir masyvų NEGALIMA.
+# a1 = random.randint(1000, 9999)
+# a2 = random.randint(1000, 9999)
+# a3 = random.randint(1000, 9999)
+# a4 = random.randint(1000, 9999)
+# a5 = random.randint(1000, 9999)
+# a6 = random.randint(1000, 9999)
+# print(a1)
+# print(a2)
+# print(a3)
+# print(a4)
+# print(a5)
+# print(a6)
+# pirmas = max(a1, a2, a3, a4, a5, a6)
+# if pirmas == a1:
+#     a1 = -1
+# elif pirmas == a2:
+#     a2 = -1
+# elif pirmas == a3:
+#     a3 = -1
+# elif pirmas == a4:
+#     a4 = -1
+# elif pirmas == a5:
+#     a5 = -1
+# elif pirmas == a6:
+#     a6 = -1
+# antras = max(a1, a2, a3, a4, a5, a6)
+# if antras == a1:
+#     a1 = -1
+# elif antras == a2:
+#     a2 = -1
+# elif antras == a3:
+#     a3 = -1
+# elif antras == a4:
+#     a4 = -1
+# elif antras == a5:
+#     a5 = -1
+# elif antras == a6:
+#     a6 = -1
+# trecias = max(a1, a2, a3, a4, a5, a6)
+# if trecias == a1:
+#     a1 = -1
+# elif trecias == a2:
+#     a2 = -1
+# elif trecias == a3:
+#     a3 = -1
+# elif trecias == a4:
+#     a4 = -1
+# elif trecias == a5:
+#     a5 = -1
+# elif trecias == a6:
+#     a6 = -1
+# ketvirtas = max(a1, a2, a3, a4, a5, a6)
+# if ketvirtas == a1:
+#     a1 = -1
+# elif ketvirtas == a2:
+#     a2 = -1
+# elif ketvirtas == a3:
+#     a3 = -1
+# elif ketvirtas == a4:
+#     a4 = -1
+# elif ketvirtas == a5:
+#     a5 = -1
+# elif ketvirtas == a6:
+#     a6 = -1
+# penktas = max(a1, a2, a3, a4, a5, a6)
+# if penktas == a1:
+#     a1 = -1
+# elif penktas == a2:
+#     a2 = -1
+# elif penktas == a3:
+#     a3 = -1
+# elif penktas == a4:
+#     a4 = -1
+# elif penktas == a5:
+#     a5 = -1
+# elif penktas == a6:
+#     a6 = -1
+# sestas = max(a1, a2, a3, a4, a5, a6)
+# print(f" Surusiuotos reiksmes: {pirmas} {antras} {trecias} {ketvirtas} {penktas} {sestas}")
